3bf39b2a8b0a915ed30da0a63a5a2334b0a54c11
lidening <282093008@qq.com>
2018-01-14 17:44:20 +0800

    几何平均值函数增加传入数组为空处理
    几何平均值函数增加传入数组为空处理

--- cpi/getGeomean.js
+++ cpi/getGeomean.js
@@ -1,13 +1,18 @@
 //arr 为求几何平均数组
 
 function getGeom(arr){
-    var a=1;
-    var geomean=1;
-    arr.forEach(function(item){
-        a*=item;
-    });
-    geomean=Math.pow(a,1/arr.length);
-    return geomean;
+    if(arr.length==0){
+        return 0;
+    }else {
+        var a=1;
+        var geomean=1;
+        arr.forEach(function(item){
+            a*=item;
+        });
+        geomean=Math.pow(a,1/arr.length);
+        return geomean;
+    }
+
 }
 
 
